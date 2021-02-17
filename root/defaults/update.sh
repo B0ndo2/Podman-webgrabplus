2324a31c38d4e0375f0209a870c40bf1e64f15e8
B0ndo2 <telerian@gmail.com>
2021-02-17 03:07:42 -0500

    add linq library

--- root/defaults/update.sh
+++ root/defaults/update.sh
@@ -3,8 +3,8 @@
 cd /app/wg++/bin || exit
 
 mono WebGrab+Plus.exe  "/config"
-if [ -f chans2correct.xml ]; then
-	mono xmltv_time_modify.exe  "/config/epg.xml" 
-fi
+#if [ -f chans2correct.xml ]; then
+	mono xmltv_time_modify.exe  "/data/epg.xml" "/data/guide.xml" "/config/chans2correct.xml"
+#fi
 
 exit 0
