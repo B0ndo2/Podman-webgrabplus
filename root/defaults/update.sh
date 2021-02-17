a1be9903f7417486f862138597b32e30307ed102
B0ndo2 <telerian@gmail.com>
2021-02-16 23:24:23 -0500

    modified update.sh

--- root/defaults/update.sh
+++ root/defaults/update.sh
@@ -4,7 +4,7 @@ cd /app/wg++/bin || exit
 
 mono WebGrab+Plus.exe  "/config"
 if [ -f chans2correct.xml ]; then
-	mono xmltv_time_modify.exe  "/config" 
+	mono xmltv_time_modify.exe  "/config/epg.xml" 
 fi
 
 exit 0
